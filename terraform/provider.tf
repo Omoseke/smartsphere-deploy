129f019dfb58dccf950ece7c313506edb7f69c59
Bonaventure Iwunor <biwunor@gmail.com>
2025-04-14 22:46:18 +0100

    Add provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+# AWS Provider Configuration
+# Note: The terraform block and provider blocks are defined in main.tf
+
+# This file is kept for documentation purposes
+# See main.tf for the actual provider configuration
