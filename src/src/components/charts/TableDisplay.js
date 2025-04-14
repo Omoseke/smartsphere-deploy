b7897661930f952c3fbe9198e716b98d86e921b7
Bonaventure Iwunor <biwunor@gmail.com>
2025-04-14 22:54:05 +0100

    Add TableDisplay.js

---
+++ src/src/components/charts/TableDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const TableDisplay = ({ data }) => {
+  return (
+    <Box sx={{ overflowX: 'auto' }}>
+      <Typography variant="body2">Table Placeholder (Would show real table in production)</Typography>
+    </Box>
+  );
+};
+
+export default TableDisplay;
