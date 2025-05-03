8ff46c85cd0cff97957daa06c6aa8e8ef630c041
Olaleye Biliki <46855599+Omoseke@users.noreply.github.com>
2025-05-03 01:10:42 -0500

    Delete server directory

--- server/db.ts
+++
@@ -1,5 +0,0 @@
-// This file is kept for reference but not actively used in the current implementation
-// Database connections are managed through the models/index.js file
-
-export const pool = null;
-export const db = null;
